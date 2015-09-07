2d0192ace7929cece23a742306cb1dfabdb45989
soka <soka@tuxhome.com.ar>
2015-09-07 16:38:55 -0300

    modifique paquetesTodos para que reciva argumentos opcionales, se puede correr ahora espcificando el timeout, que lea de un archivo de captura o que escriba la captura a archivo solamente. Tambien cambie para que devuelva los nombres de los simbolos en lugar de su valor en HEX

--- tp1/codigo/paquetesTodos.py
+++ tp1/codigo/paquetesTodos.py
@@ -1,12 +1,15 @@
 #! /usr/bin/python
 from scapy.all import *
 from scapy.utils import rdpcap
+from scapy.utils import wrpcap
 from math import log
 import sys
 import os
 import operator
+import argparse
 
 htypes = {}
+offline = False
 
 def entropia(fuente):
     N = sum(fuente.values())
@@ -27,35 +30,59 @@ def prob_simbolo(diccionario, key):
 
 def monitor_callback_all(pkt):
     if Ether in pkt:
-        htype = hex(pkt.type)
-            
+#        htype = hex(pkt.type)
+        if pkt.type == 34525:
+            htype = 'IPv6'
+        else:
+            htype = pkt.payload.name
         # Modelo fuente de informacion hw type
 	htypes[htype] = htypes.get(htype, 0.0) + 1.0  
 
 def informacion(diccionario):
-    for simbolo   in diccionario.keys():
-	     print "simbolo : %s, Informacion : %f" % (simbolo, info_simbolo(diccionario, simbolo))
+    for simbolo in diccionario.keys():
+	    print "simbolo : %4s, Informacion : %f" % (simbolo, info_simbolo(diccionario, simbolo))
 
 def probabilidad(diccionario):
-    for simbolo   in diccionario.keys():
-	     print "simbolo : %s, Probabilidad : %f" % (simbolo, prob_simbolo(diccionario, simbolo))
+    for simbolo in diccionario.keys():
+        print "simbolo : %4s, Probabilidad : %f" % (simbolo, prob_simbolo(diccionario, simbolo))
 
 def sniff_offline(archivo):
     pkts=rdpcap(archivo);
-    for pkt   in pkts:
-             monitor_callback_all(pkt);
+    for pkt in pkts:
+        monitor_callback_all(pkt);
 
-if len(sys.argv) < 2:
-    #Si no se especifico archivo ejecutamos el sniff online
-    sniff(prn=arp_monitor_callback, store=0)
-elif not os.path.exists(sys.argv[1]):
-    sys.stderr.write('ERROR: no existe el archivo pasado como parametro\n')
+def valid_file(parser, arg):
+    global offline
+    if not os.path.isfile(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        offline = True
+        return arg
+
+parser = argparse.ArgumentParser(description='''Network traffic capture tool.''')
+parser.add_argument('-t', '--time', required=False, type=int, help='max running time in seconds', default=60)
+parser.add_argument('-i', '--input', required=False, type=lambda x: valid_file(parser,x), help='input filename for offline sniff', metavar='FILE')
+parser.add_argument('-d', '--dump', required=False, type=str, help='capture packets and dump them to output file', metavar='FILE')
+args = parser.parse_args()
+
+print '\n' + "Running %s with parameters: time=%s, input=%s"%(os.path.basename(sys.argv[0]) ,args.time, args.input) + '\n'
+
+if offline:
+    #realizamos un sniff offline
+    sniff_offline(args.input)
+elif not args.dump == None:
+    #dump to file
+    pkts = sniff(timeout=args.time)
+    wrpcap(args.dump,pkts)
+    print 'output saved in file {}'.format(args.dump)
+    exit(0)
 else:
-    #Si existe el archivo realizamos un sniff offline
-    sniff_offline(sys.argv[1]);
-    print "Entropia Fuente HW Type: %f" % entropia(htypes)
-    print probabilidad(htypes)
-    print informacion(htypes)
+    #ejecutamos el sniff online
+    sniff(prn=monitor_callback_all, store=0, timeout=args.time)
+
+print "Entropia Fuente HW Type: %f" % entropia(htypes) + '\n'
+print probabilidad(htypes)
+print informacion(htypes)
 
 
 
